26a48f1c0be05fe69b24b50ebe197574bd2df122
Mann U <mannu@Manassehs-Laptop.local>
2023-03-16 11:51:08 +0000

    all reducers done and ready to assign events

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -7,6 +7,7 @@ import {
   Pressable,
   Image,
   TextInput,
+  ScrollView,
 } from "react-native";
 import React, { useEffect, useState } from "react";
 import * as Location from "expo-location";
@@ -14,9 +15,13 @@ import { MaterialIcons } from "@expo/vector-icons";
 import { Feather } from "@expo/vector-icons";
 import Carousel from "../components/Carousel";
 import Services from "../components/Services";
- 
+import DressItem from "../components/DressItem";
+import { useDispatch, useSelector } from "react-redux";
+import { getProducts } from "../ProductReducer";
 
 const HomeScreen = () => {
+  const cart = useSelector((state) => state.cart.cart);
+
   const [displayCurrentAddress, setDisplayCurrentAddress] = useState(
     "we are loading your location"
   );
@@ -80,8 +85,73 @@ const HomeScreen = () => {
     }
   };
 
+  /* Accessing products using redux toolkitz */
+  const product = useSelector((state) => state.product.product);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    if (product.length > 0) return;
+    const fetchProducts = () => {
+      services.map((service) => dispatch(getProducts(service)));
+    };
+    fetchProducts();
+  }, []);
+  console.log(product);
+
+  // products data
+  const services = [
+    {
+      id: "0",
+      image: "https://cdn-icons-png.flaticon.com/128/4643/4643574.png",
+      name: "shirt",
+      quantity: 0,
+      price: 10,
+    },
+    {
+      id: "11",
+      image: "https://cdn-icons-png.flaticon.com/128/892/892458.png",
+      name: "T-shirt",
+      quantity: 0,
+      price: 10,
+    },
+    {
+      id: "12",
+      image: "https://cdn-icons-png.flaticon.com/128/9609/9609161.png",
+      name: "dresses",
+      quantity: 0,
+      price: 10,
+    },
+    {
+      id: "13",
+      image: "https://cdn-icons-png.flaticon.com/128/599/599388.png",
+      name: "jeans",
+      quantity: 0,
+      price: 10,
+    },
+    {
+      id: "14",
+      image: "https://cdn-icons-png.flaticon.com/128/9431/9431166.png",
+      name: "Sweater",
+      quantity: 0,
+      price: 10,
+    },
+    {
+      id: "15",
+      image: "https://cdn-icons-png.flaticon.com/128/3345/3345397.png",
+      name: "shorts",
+      quantity: 0,
+      price: 10,
+    },
+    {
+      id: "16",
+      image: "https://cdn-icons-png.flaticon.com/128/293/293241.png",
+      name: "Sleeveless",
+      quantity: 0,
+      price: 10,
+    },
+  ];
+
   return (
-    <SafeAreaView style={{backgroundColor:"#f0f0f0",flex:1}}>
+    <ScrollView style={{ backgroundColor: "#f0f0f0", flex: 1, marginTop: 50 }}>
       {/* Location and Profile */}
       <View style={{ flexDirection: "row", alignItems: "center", padding: 10 }}>
         <MaterialIcons name="location-on" size={30} color="#fd5c63" />
@@ -107,9 +177,9 @@ const HomeScreen = () => {
           flexDirection: "row",
           alignItems: "center",
           justifyContent: "space-between",
-          borderWidth:0.8,
-          borderColor:"#c0c0c0",
-          borderRadius:7
+          borderWidth: 0.8,
+          borderColor: "#c0c0c0",
+          borderRadius: 7,
         }}
       >
         <TextInput placeholder="search items or more"></TextInput>
@@ -117,11 +187,15 @@ const HomeScreen = () => {
       </View>
 
       {/* Image Carousel */}
-      <Carousel/>
+      <Carousel />
 
       {/* Services Component */}
-      <Services/>
-    </SafeAreaView>
+      <Services />
+      {/* render all the products */}
+      {product.map((item, index) => (
+        <DressItem item={item} key={index} />
+      ))}
+    </ScrollView>
   );
 };
 
